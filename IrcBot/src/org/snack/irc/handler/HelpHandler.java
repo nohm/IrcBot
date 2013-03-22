83e3a5ff3853da107ea9b59acca1e9ab568c9eea
Steven Koolhaas <snack93@lavabit.com>
2013-03-22 17:03:12 +0100

    Forgot file.

---
+++ IrcBot/src/org/snack/irc/handler/HelpHandler.java
@@ -0,0 +1,29 @@
+package org.snack.irc.handler;
+
+import org.pircbotx.hooks.events.MessageEvent;
+import org.snack.irc.model.Chan;
+import org.snack.irc.settings.Configuration;
+
+public class HelpHandler {
+
+	// TODO: DOCS
+	public static void sendHelp(MessageEvent<?> event) {
+		Chan chan = Configuration.CHANNELS.get(event.getChannel().getName());
+		event.getBot().sendMessage(event.getUser(), "My commands:");
+		if (chan.getWeather()) {
+			event.getBot().sendNotice(event.getUser(), "Get weather: .we/,we/!we [name] (Name gets stored)");
+		}
+		if (chan.getLastfm()) {
+			event.getBot().sendNotice(event.getUser(), "Get lastfm: .np/,np/!np [name] (Name gets stored)");
+		}
+		if (chan.getHtml()) {
+			event.getBot().sendNotice(event.getUser(), "Auto respond to http(s):// links");
+		}
+		if (chan.getQuote()) {
+			event.getBot().sendNotice(event.getUser(), "Quotes: .quote/,quote/!quote [name] (Name is optional)");
+		}
+		if (chan.getTell()) {
+			event.getBot().sendNotice(event.getUser(), "Tell someone on join: .tell/,tell/!tell [message]");
+		}
+	}
+}
