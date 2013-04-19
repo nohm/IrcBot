ec5b3e012c6275987cc77015c263bc199e7ca311
Steven Koolhaas <snack93@lavabit.com>
2013-04-19 15:03:51 +0200

    Massive 4chan api cleanup..

--- IrcBot/src/irc/worker/FourChanAPI.java
+++ IrcBot/src/irc/worker/FourChanAPI.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.util.ArrayList;
 
 import net.sf.json.JSONArray;
-import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
@@ -22,56 +21,6 @@ import org.jsoup.safety.Whitelist;
 
 public class FourChanAPI {
 
-	public static FThread retrieve(String board, String threadNumber) throws Exception {
-		ByteArrayOutputStream output = Utils.httpRequest("https://api.4chan.org/" + board.toLowerCase() + "/res/" + threadNumber + ".json");
-		JSONObject jo = (JSONObject) JSONSerializer.toJSON(output.toString().replace("&gt;", ">").replace("&lt;", "<"));
-
-		JSONArray posts = jo.getJSONArray("posts");
-		JSONObject thread = posts.getJSONObject(0);
-		String subject;
-		String post;
-		String number;
-		String name;
-		String comment;
-		String replies;
-		try {
-			subject = Jsoup.clean(thread.getString("sub"), Whitelist.simpleText()).toString();
-			subject = subject.replace("&gt;", ">").replace("&lt;", "<");
-		} catch (JSONException e) {
-			subject = "n/a";
-		}
-		try {
-			post = Jsoup.clean(thread.getString("com"), Whitelist.simpleText()).toString();
-		} catch (JSONException e) {
-			post = "n/a";
-		}
-		try {
-			number = Jsoup.clean(thread.getString("no"), Whitelist.simpleText()).toString();
-		} catch (JSONException e) {
-			number = "n/a";
-		}
-		try {
-			name = Jsoup.clean(thread.getString("name"), Whitelist.simpleText()).toString();
-		} catch (JSONException e) {
-			name = "n/a";
-		}
-		try {
-			comment = Jsoup.clean(thread.getString("com"), Whitelist.simpleText()).toString();
-			comment = comment.replace("&gt;", ">").replace("&lt;", "<");
-			if (comment.length() > 100) {
-				comment = comment.substring(0, 96) + "...";
-			}
-		} catch (JSONException e) {
-			comment = "n/a";
-		}
-		try {
-			replies = thread.getString("replies");
-		} catch (JSONException e) {
-			replies = "n/a";
-		}
-		return new FThread(name, subject, post, new URI("https://boards.4chan.org/" + board.toLowerCase() + "/res/" + number).toString(), comment, replies);
-	}
-
 	public static FThread[] search(String board, String term) throws Exception {
 		ArrayList<FThread> threadList = new ArrayList<FThread>();
 		String cleanedTerm = term.toLowerCase();//Utils.encodeQuery(term, true);
@@ -84,62 +33,40 @@ public class FourChanAPI {
 			JSONArray threads = page.getJSONArray("threads");
 			for (int j = 0; j < threads.size(); j++) {
 				JSONObject thread = threads.getJSONObject(j);
-				String subject;
-				String post;
-				String number;
-				String name;
-				String comment;
-				String replies;
+				FThread parsed = parseThread(board, thread);
 				boolean ignore = false; // Ignore sticky/closed threads
-				try {
-					subject = Jsoup.clean(thread.getString("sub"), Whitelist.simpleText()).toString();
-				} catch (JSONException e) {
-					subject = "n/a";
-				}
-				try {
-					post = Jsoup.clean(thread.getString("com"), Whitelist.simpleText()).toString();
-				} catch (JSONException e) {
-					post = "n/a";
-				}
-				try {
-					number = Jsoup.clean(thread.getString("no"), Whitelist.simpleText()).toString();
-				} catch (JSONException e) {
-					number = "n/a";
-				}
-				try {
-					name = Jsoup.clean(thread.getString("name"), Whitelist.simpleText()).toString();
-				} catch (JSONException e) {
-					name = "n/a";
-				}
-				try {
-					comment = Jsoup.clean(thread.getString("com"), Whitelist.simpleText()).toString();
-					if (comment.length() > 100) {
-						comment = comment.substring(0, 96) + "...";
-					}
-				} catch (JSONException e) {
-					comment = "n/a";
-				}
-				try {
-					replies = thread.getString("replies");
-				} catch (JSONException e) {
-					replies = "n/a";
-				}
-				try {
-					ignore = thread.getInt("sticky") == 1;
-				} catch (JSONException e) {
-				}
-				try {
-					ignore = thread.getInt("closed") == 1;
-				} catch (JSONException e) {
-				}
+				ignore = thread.has("sticky") ? thread.getInt("sticky") == 1 : ignore;
+				ignore = thread.has("closed") ? thread.getInt("closed") == 1 : ignore;
 				if (!ignore
-						&& (subject.toLowerCase().contains(cleanedTerm) || subject.toLowerCase().equals(cleanedTerm) || post.toLowerCase().contains(cleanedTerm)
-								|| post.toLowerCase().equals(cleanedTerm) || name.toLowerCase().contains(cleanedTerm) || name.toLowerCase().equals(cleanedTerm))) {
-					threadList.add(new FThread(name, subject, post, new URI("https://boards.4chan.org/" + board.toLowerCase() + "/res/" + number).toString(), comment, replies));
+						&& (parsed.subject.toLowerCase().contains(cleanedTerm) || parsed.subject.toLowerCase().equals(cleanedTerm) || parsed.post.toLowerCase().contains(cleanedTerm)
+								|| parsed.post.toLowerCase().equals(cleanedTerm) || parsed.name.toLowerCase().contains(cleanedTerm) || parsed.name.toLowerCase().equals(cleanedTerm))) {
+					threadList.add(parsed);
 				}
 			}
 		}
 
 		return threadList.toArray(new FThread[threadList.size()]);
+	}
+
+	public static FThread retrieve(String board, String threadNumber) throws Exception {
+		ByteArrayOutputStream output = Utils.httpRequest("https://api.4chan.org/" + board.toLowerCase() + "/res/" + threadNumber + ".json");
+		JSONObject jo = (JSONObject) JSONSerializer.toJSON(output.toString().replace("&gt;", ">").replace("&lt;", "<"));
+
+		JSONArray posts = jo.getJSONArray("posts");
+		JSONObject thread = posts.getJSONObject(0);
+		return parseThread(board, thread);
+	}
+
+	private static FThread parseThread(String board, JSONObject thread) throws Exception {
+		String subject = thread.has("sub") ? Jsoup.clean(thread.getString("sub"), Whitelist.simpleText()).toString() : "n/a";
+		String post = thread.has("com") ? Jsoup.clean(thread.getString("com"), Whitelist.simpleText()).toString() : "n/a";
+		String number = thread.has("no") ? Jsoup.clean(thread.getString("no"), Whitelist.simpleText()).toString() : "n/a";
+		String name = thread.has("name") ? Jsoup.clean(thread.getString("name"), Whitelist.simpleText()).toString() : "n/a";
+		String comment = thread.has("com") ? Jsoup.clean(thread.getString("com"), Whitelist.simpleText()).toString() : "n/a";
+		String replies = thread.has("replies") ? thread.getString("replies") : "n/a";
+		if (comment.length() > 100) {
+			comment = comment.substring(0, 96) + "...";
+		}
+		return new FThread(name, subject, post, new URI("https://boards.4chan.org/" + board.toLowerCase() + "/res/" + number).toString(), comment, replies);
 	}
 }
